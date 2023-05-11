b1e728fe8c592e632667573f5e8d0c6373a97e4a
Robert Rowe <1669080+CaptainJet@users.noreply.github.com>
2023-05-11 14:14:19 -0700

    Fix getting module, args -> arg

--- src/mruby-emscripten-functions.c
+++ src/mruby-emscripten-functions.c
@@ -4,12 +4,12 @@
 
 void mrb_real_main_loop(mrb_state *mrb)
 {
-  mrb_funcall(mrb, Mruby_platform_module, "main_loop", 0);
+  mrb_funcall(mrb, mrb_class_get(mrb, "Platform"), "main_loop", 0);
 }
 
 mrb_value mrb_set_main_loop(mrb_state *mrb, mrb_value self)
 {
-  emscripten_set_main_loop_args(mrb_real_main_loop, mrb, 0, 1);
+  emscripten_set_main_loop_arg(mrb_real_main_loop, mrb, 0, 1);
   return mrb_nil_value();
 }
 #endif
@@ -17,7 +17,7 @@ mrb_value mrb_set_main_loop(mrb_state *mrb, mrb_value self)
 void mrb_mruby_emscripten_functions_gem_init(mrb_state *mrb)
 {
 #ifdef __EMSCRIPTEN__
-  mrb_define_module_function(mrb, Mruby_platform_module, "set_main_loop", mrb_set_main_loop, MRB_ARGS_NONE());
+  mrb_define_module_function(mrb, mrb_class_get(mrb, "Platform"), "set_main_loop", mrb_set_main_loop, MRB_ARGS_NONE());
 #endif
 }
 
